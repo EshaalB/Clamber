7da27f4ac41a96ee8fb6d67581484c1fea086e05
Eshaal B <111786372+EshaalB@users.noreply.github.com>
2026-05-10 23:14:38 +0500

    add root index.js bridge to simplify deployment entry point

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Root Bridge for Monorepo Deployment
+ * This file allows hosting providers to find the entry point 
+ * when they are limited to the root directory.
+ */
+require('./server/src/server.js');
